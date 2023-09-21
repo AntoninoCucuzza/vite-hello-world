affa50c2801be423c2a77396910fbb901021999e
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-09-21 16:22:45 +0200

    add bonus1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script >
-import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './/components/AppTitle.vue';
 
 export default {
   data() {
@@ -7,10 +7,14 @@ export default {
       message: 'hello Vite and Vue3!',
     }
   },
+  components: {
+    AppTitle
+  }
 }
 </script>
 
 <template>
+  <AppTitle />
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script></script>
+
+<template>
+    <div id="title">
+        <h1 id="text">La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<style>
+#title {
+    padding: 1rem;
+    border-radius: 20px;
+    background: linear-gradient(120deg, #540d6e, #ee4266, #ffd23f, #3bceac, #0ead69);
+    transition: all 0.3s;
+}
+
+#title:hover {
+    filter: drop-shadow(0 0 1.5rem rgb(61, 60, 60));
+}
+
+#text {
+    background: linear-gradient(160deg, #0ead69, #3bceac, #1781b6, #ee4266, #540d6e);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+}
+</style>
